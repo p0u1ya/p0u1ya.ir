37ba279097793ba634c9e5d6dfc7cbecfdede58c
Pourya <68809310+p0u1ya@users.noreply.github.com>
2024-03-10 22:49:36 +0330

    Update seo.ts

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -8,22 +8,22 @@ export function useSeoProps(
 ): Partial<ComponentProps<typeof NextSeo>> {
 	const router = useRouter();
 
-	const title = 'nuro ─ developer';
-	const description = "Hey 👋 I'm Ben, a developer";
+	const title = 'p0u1ya ─ developer';
+	const description = "Hey 👋 I'm Pourya, a developer";
 
 	return {
 		title,
 		description,
-		canonical: `https://nuro.dev/${router.asPath}`,
+		canonical: `https://p0u1ya.ir/${router.asPath}`,
 		openGraph: {
 			title,
 			description,
-			site_name: 'nuro',
-			url: `https://nuro.dev/${router.asPath}`,
+			site_name: 'p0u1ya',
+			url: `https://p0u1ya.ir/${router.asPath}`,
 			type: 'website',
 			images: [
 				{
-					url: 'https://nuro.dev/banner.png',
+					url: 'https://p0u1ya.ir/banner.png',
 					alt: description,
 					width: 1280,
 					height: 720,
@@ -32,8 +32,8 @@ export function useSeoProps(
 		},
 		twitter: {
 			cardType: 'summary_large_image',
-			handle: '@nurodev',
-			site: '@nurodev',
+			handle: '@p0u1ya',
+			site: '@p0u1ya',
 		},
 		...props,
 	};
